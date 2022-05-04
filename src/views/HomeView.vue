93541c8703df1b601e97457a0bdb5d0fd332ede6
Baha <temirhan020594@mail.com>
2022-05-04 13:53:52 +0600

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home w-full min-h-screen bg-gray-200 p-10">
+    <div class="flex flex-wrap gap-10">
+      <div class=" bg-slate-600 w-60 p-5 flex-1  shadow-md rounded-lg mb-5" v-for="item, n in 4" :key="n">
+        Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Продолжил, встретил несколько вскоре скатился дороге коварных осталось оксмокс решила напоивший но приставка заглавных заманивший деревни по всей журчит свою которое.
+      </div>
+    </div>
+    <div class="text-red-500 ">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde possimus omnis repellat dicta quis, aperiam ducimus libero explicabo voluptate dolore ea ipsum magni amet quisquam laudantium sunt ratione, quam veniam.
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {},
 }
 </script>
